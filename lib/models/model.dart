149ca121c9bed1c808331a5952c82f2d356f0a06
kenan7Th <thbeankenan@gmail.com>
2024-01-19 05:09:09 -0800

    second

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -4,19 +4,31 @@ class ProductModel {
   final double price;
   final String description;
   final String image;
+  final RatingModel rating;
   ProductModel({
     required this.id,
     required this.title,
     required this.price,
     required this.description,
     required this.image,
+    required this.rating,
   });
-  factory ProductModel.fromJsom(jsonData) {
+  factory ProductModel.fromJson(jsonData) {
     return ProductModel(
         id: jsonData['id'],
         title: jsonData['title'],
         price: jsonData['price'],
         description: jsonData['description'],
-        image: jsonData['image']);
+        image: jsonData['image'],
+        rating: RatingModel.fromJson(jsonData['rating']));
+  }
+}
+
+class RatingModel {
+  final double rate;
+  final int count;
+  RatingModel({required this.rate, required this.count});
+  factory RatingModel.fromJson(jsonData) {
+    return RatingModel(rate: jsonData['rate'], count: jsonData['count']);
   }
 }
