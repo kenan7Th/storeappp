d5181bb7a2ee1a52c15d374379da7750c3113979
kenan7Th <thbeankenan@gmail.com>
2024-01-28 23:32:22 -0800

    newcommit

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,11 +1,13 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<http.Response> get({required String myurl}) async {
+  Future<dynamic> get({required String myurl}) async {
     http.Response response = await http.get(Uri.parse(myurl));
 
     if (response.statusCode == 200) {
-      return response;
+      return jsonDecode(response.body);
     } else {
       throw Exception('There is a problem with the status code');
     }
--- lib/services/get_all_products_service.dart
+++ lib/services/get_all_products_service.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-
-import 'package:http/http.dart';
 import 'package:storeapp/helper/api.dart';
 import 'package:storeapp/models/model.dart';
 
@@ -8,17 +5,13 @@ class AllProuctsService {
   Future<List<ProductModel>> getAllProducts() async {
     // // ignore: unused_local_variable
     // Response response = await get(Uri.parse('https://fakestores.com/products'));
-    Response response =
+    List<dynamic> data =
         await Api().get(myurl: 'https://fakestores.com/products');
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
-      List<ProductModel> productList = [];
-      for (int i = 0; i < data.length; i++) {
-        productList.add(ProductModel.fromJson(data[i]));
-      }
-      return productList;
-    } else {
-      throw Exception('there is a problem with status code ');
+
+    List<ProductModel> productList = [];
+    for (int i = 0; i < data.length; i++) {
+      productList.add(ProductModel.fromJson(data[i]));
     }
+    return productList;
   }
 }
